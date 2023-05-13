de6d4bcbc21d82925517ce4cea5e4e1fc402a333
David Kulhánek <davidkulh182@gmail.com>
2023-05-13 16:12:28 +0200

    Update lint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,38 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
+  env: {
+    node: true,
+  },
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', 'prettier'],
+  plugins: ['prettier'],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    yoda: [
+      'warn',
+      'always',
+      {
+        exceptRange: true,
+        onlyEquality: true,
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 120,
+        singleQuote: true,
+        trailingComma: 'all',
+        bracketSpacing: true,
+        arrowParens: 'always',
+        tabWidth: 2,
+        semi: true,
+      },
+    ],
+  },
+};
