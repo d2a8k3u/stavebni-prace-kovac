e990562359275ad2350317424afd539c17e44ceb
David Kulhánek <davidkulh182@gmail.com>
2023-05-14 00:58:25 +0200

    Add i18n

---
+++ src/plugins/i18n.js
@@ -0,0 +1,11 @@
+import { createI18n } from 'vue-i18n';
+
+import cs from '@/locales/cs.json';
+
+export default createI18n({
+  locale: 'cs',
+  fallbackLocale: 'cs',
+  messages: {
+    cs,
+  },
+});
