3ed2f40290c4d51fad56692a0990e1323759594d
David Kulhánek <davidkulh182@gmail.com>
2023-05-14 00:58:25 +0200

    Add i18n

--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
-import './styles/index.css';
+import './assets/index.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 import { createApp } from 'vue';
 
 import { createPinia } from 'pinia';
+import i18n from '@/plugins/i18n';
 import App from './App.vue';
 
 import router from './router';
 
 const app = createApp(App);
-app.use(createPinia());
 
+app.use(createPinia());
 app.use(router);
+app.use(i18n);
 
 app.mount('#app');
